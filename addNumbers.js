28399757905ff09377339d27f2b1b1eaa9b1885e
Abeer Kapoor <abrkapoor@gmail.com>
2025-03-18 18:52:23 +0530

    dlt

---
+++ addNumbers.js
@@ -0,0 +1,8 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+// Example usage
+const num1 = 5;
+const num2 = 10;
+console.log(`The sum of ${num1} and ${num2} is ${addTwoNumbers(num1, num2)}`);
